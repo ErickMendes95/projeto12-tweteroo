ea88ad14506fbaac2870011595c81ce32f4a79b2
Erick Mendes <erickmendesweb@gmail.com>
2023-01-07 15:52:08 -0300

    comandos post criado para /sign-up e /tweets

--- src/app.js
+++ src/app.js
@@ -6,12 +6,28 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post("/sign-up", (req, res) => {
+const arrayUsuarios = []
+const arrayTweets = []
 
+app.post("/sign-up", (req, res) => {
+    const userinfo = req.body
+    arrayUsuarios.push(userinfo)
+    console.log(arrayUsuarios)
+
+    res.status(201).send("OK")
 })
 
 app.post("/tweets", (req, res) => {
+    const { username } = req.body
+    const { tweet } = req.body
+    
+    const usuario = arrayUsuarios.find(item => item.username === username)
 
+    if(!usuario){
+        res.status(401).send("UNAUTHORIZED")
+    }
+    arrayTweets.push(tweet)
+    res.status(201).send("OK")
 })
 
 app.get("/tweets", (req, res) => {
